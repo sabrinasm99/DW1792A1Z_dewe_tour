dc75f13d5367f4252e5ed24cc872b1dbd9d71e52
Sabrina Sri Mulyani <sabrinasrimulyani@gmail.com>
2020-08-07 00:52:57 +0700

    add register

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -5,24 +5,65 @@ import palmmodal from "../image/palmmodal.svg";
 
 function HeaderHome() {
   const [showModalLogin, setShowModalLogin] = useState(false);
+  const [showModalRegister, setShowModalRegister] = useState(false);
   const [inputLogin, setInputLogin] = useState({
     email: "",
     password: "",
   });
+  const [inputRegister, setInputRegister] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+    phone: "",
+    address: "",
+  });
+  const [warning, setWarning] = useState("");
   const handleChangeLogin = (event) => {
     setInputLogin({
       ...inputLogin,
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleChangeRegister = (event) => {
+    setInputRegister({
+      ...inputRegister,
+      [event.target.name]: event.target.value,
+    });
+  };
   const submitLogin = (event) => {
     event.preventDefault();
-    localStorage.setItem('email', inputLogin.email);
-    alert('Success Login');
+    localStorage.setItem("email", inputLogin.email);
+    alert("Success Login");
     setShowModalLogin(false);
-  }
+  };
+  const submitRegister = (event) => {
+    event.preventDefault();
+    if (!inputRegister.fullname) {
+      setWarning("Please fill out this field");
+    } else {
+      alert("Success Register");
+      setShowModalRegister(false);
+    }
+  };
   const closeModalLogin = () => {
     setShowModalLogin(false);
+    const newInputLogin = {
+      email: "",
+      password: "",
+    };
+    setInputLogin(newInputLogin);
+  };
+  const closeModalRegister = () => {
+    setShowModalRegister(false);
+    const newInputRegister = {
+      fullname: "",
+      email: "",
+      password: "",
+      phone: "",
+      address: "",
+    };
+    setInputRegister(newInputRegister);
+    setWarning("");
   };
 
   let modalLogin = null;
@@ -38,7 +79,7 @@ function HeaderHome() {
             left: "50%",
             top: "50%",
             width: "350px",
-            boxShadow: "1px 1px 8px black"
+            boxShadow: "1px 1px 8px black",
           }}
           className="rounded-md py-3 px-6"
         >
@@ -109,6 +150,140 @@ function HeaderHome() {
         />
       </React.Fragment>
     );
+
+  let modalRegister = null;
+  if (showModalRegister)
+    modalRegister = (
+      <React.Fragment>
+        <div
+          style={{
+            position: "fixed",
+            zIndex: 200,
+            transform: "translate(-50%,-50%)",
+            backgroundColor: "white",
+            left: "50%",
+            top: "50%",
+            width: "350px",
+            boxShadow: "1px 1px 8px black",
+          }}
+          className="rounded-md py-3 px-6"
+        >
+          <h1 className="text-black text-3xl font-bold mt-8">Register</h1>
+          <div className="mt-12">
+            <label className="text-black block text-left font-bold">
+              Full Name
+            </label>
+            <input
+              className="text-gray-800 w-full border pl-1 focus:outline-none rounded"
+              style={{
+                backgroundColor: "rgba(210, 210, 210, 0.25)",
+                borderColor: "rgba(210, 210, 210, 0.25)",
+              }}
+              type="text"
+              name="fullname"
+              value={inputRegister.fullname}
+              onChange={handleChangeRegister}
+            />
+            <h3 className="text-red-600 text-left text-sm">{warning}</h3>
+          </div>
+          <div className="mt-5">
+            <label className="text-black block text-left font-bold">
+              Email
+            </label>
+            <input
+              className="text-gray-800 w-full border pl-1 focus:outline-none rounded"
+              style={{
+                backgroundColor: "rgba(210, 210, 210, 0.25)",
+                borderColor: "rgba(210, 210, 210, 0.25)",
+              }}
+              type="email"
+              name="email"
+              value={inputRegister.email}
+              onChange={handleChangeRegister}
+            />
+          </div>
+          <div className="mt-5">
+            <label className="text-black block text-left font-bold">
+              Password
+            </label>
+            <input
+              className="text-gray-800 w-full border pl-1 focus:outline-none rounded"
+              style={{
+                backgroundColor: "rgba(210, 210, 210, 0.25)",
+                borderColor: "rgba(210, 210, 210, 0.25)",
+              }}
+              type="password"
+              name="password"
+              value={inputRegister.password}
+              onChange={handleChangeRegister}
+            />
+          </div>
+          <div className="mt-5">
+            <label className="text-black block text-left font-bold">
+              Phone
+            </label>
+            <input
+              className="text-gray-800 w-full border pl-1 focus:outline-none rounded"
+              style={{
+                backgroundColor: "rgba(210, 210, 210, 0.25)",
+                borderColor: "rgba(210, 210, 210, 0.25)",
+              }}
+              type="number"
+              name="phone"
+              value={inputRegister.phone}
+              onChange={handleChangeRegister}
+            />
+          </div>
+          <div className="mt-5">
+            <label className="text-black block text-left font-bold">
+              Address
+            </label>
+            <textarea
+              className="text-gray-800 w-full border pl-1 focus:outline-none rounded"
+              style={{
+                backgroundColor: "rgba(210, 210, 210, 0.25)",
+                borderColor: "rgba(210, 210, 210, 0.25)",
+              }}
+              name="address"
+              value={inputRegister.address}
+              onChange={handleChangeRegister}
+            />
+          </div>
+          <div className="mt-8">
+            <button
+              onClick={submitRegister}
+              className="w-full p-1 px-3 bg-purple-800 text-white focus:outline-none border rounded font-bold"
+              style={{ backgroundColor: "#FFAF00", borderColor: "#FFAF00" }}
+            >
+              Register
+            </button>
+          </div>
+          <h3 className="text-sm mt-3 font-light" style={{ color: "#B1B1B1" }}>
+            Don't have an account? Click{" "}
+            <h3 className="font-bold inline">Here</h3>
+          </h3>
+          <div className="absolute" style={{ top: 0, left: 0 }}>
+            <img src={palmmodal} />
+          </div>
+          <div className="absolute" style={{ top: 0, right: 0 }}>
+            <img src={hibiscusmodal} className="rounded-md" />
+          </div>
+        </div>
+        <div
+          onClick={closeModalRegister}
+          style={{
+            position: "fixed",
+            zIndex: 199,
+            top: 0,
+            left: 0,
+            height: "100%",
+            width: "100%",
+            backgroundColor: "rgba(0,0,0,0.5)",
+          }}
+        />
+      </React.Fragment>
+    );
+
   return (
     <React.Fragment>
       <div
@@ -131,6 +306,7 @@ function HeaderHome() {
             </div>
             <div className="flex items-center">
               <button
+                onClick={() => setShowModalRegister(true)}
                 className="py-1 px-6 font-medium border text-sm rounded-md"
                 style={{ backgroundColor: "#FFAF00", borderColor: "#FFAF00" }}
               >
@@ -170,6 +346,7 @@ function HeaderHome() {
         </div>
       </div>
       {modalLogin}
+      {modalRegister}
     </React.Fragment>
   );
 }
